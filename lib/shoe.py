111d58cdf9a61eb48be77904f238b5523199dc03
George-Okumu <gokumu12@gmail.com>
2023-08-28 11:08:13 +0300

    done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python3
 
-class Shoe:    
-    def __init__(self, brand, size, condition="New"):
-        self.brand = brand
+class Shoe:
+    def __init__(self, brand, size):
         self.size = size
-        self.condition = condition
+        self.brand = brand
 
-    def cobble(self):
-        print("Your shoe is as good as new!")
+    @property
+    def size(self):
+        return self._size
     
-    def set_size(self, size):
+    @size.setter
+    def size(self, size):
         if isinstance(size, int):
             self._size = size
         else:
             print("size must be an integer")
 
-    def get_size(self):
-        return self._size
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!") 
+    # def __init__(self, brand, size, condition="New"):
+    #     self.brand = brand
+    #     self.size = size
+    #     self.condition = condition
+
+    # def cobble(self):
+    #     print("Your shoe is as good as new!")
     
-    # def set_condition(self, condition):
-    #     self._condition = condition
+    # def set_size(self, size):
+    #     if isinstance(size, int):
+    #         self._size = size
+    #     else:
+    #         print("size must be an integer")
 
-    # def get_condition(self):
-    #     return self._condition
+    # def get_size(self):
+    #     return self._size
+    
+    # # def set_condition(self, condition):
+    # #     self._condition = condition
 
-    size = property(get_size, set_size)
-    # condition = property(get_condition, set_condition, "New")
+    # # def get_condition(self):
+    # #     return self._condition
+
+    # size = property(get_size, set_size)
+    # # condition = property(get_condition, set_condition, "New")
